81134927a68a6810b7356929c4e7f60ae5344312
thara0402 <thara0402@gmail.com>
2018-08-04 14:37:42 +0900

    create project.

---
+++ test/Services/Goro.Api.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Goro.Api.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ test/Web/Goro.Mvc.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Goro.Mvc.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
